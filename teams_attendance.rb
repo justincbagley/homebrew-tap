cfa3ea940629b613490ca240b8b71d8d59e7dea6
Justin C. Bagley <justincbagley@users.noreply.github.com>
2020-12-14 14:03:09 -0600

    Update teams_attendance.rb

--- teams_attendance.rb
+++ teams_attendance.rb
@@ -1,4 +1,4 @@
-class Teams_Attendance < Formula
+class TeamsAttendance < Formula
 
   desc "Bash script to summarize Microsoft Teams Participant attendance data"
   homepage "https://github.com/justincbagley/teams-attendance"
